b29b16e1300dcd8a5af60a0d5e24379916932b28
hopelesscaffeineaddict <rayneorshine03@gmail.com>
2026-03-15 05:05:10 +1100

    error logging in /scan route for invalid request/request body/url

--- backend/app.py
+++ backend/app.py
@@ -106,17 +106,36 @@ def check_url():
     Returns:
         return: The results if successful otherwise return 400 error
     """
-    if not isinstance(request.json, dict):
+
+    # check whether request is JSON
+    if not request.is_json:
+        msg = f"Invalid request from {request.remote_addr}: Not a JSON request"
+        app.logger.warning(msg)
+        write_log(msg, "ERROR")
+        return jsonify({"error": "send JSON request"}), 400
+
+    # check whether request body is a JSON object 
+    request_data = request.get_json()
+    if not isinstance(request_data, dict):
+        msg = f"{request.remote_addr}: Body is not JSON object"
+        app.logger.warning(msg)
+        write_log(msg, "ERROR")
         return jsonify({"error": "invalid request body"}), 400
 
-    request_data = request.json
+    # check if url field missing or not a string 
     url = request_data.get("url")
-    if url is None:
-        return jsonify({"error": "invalid request body"}), 400
-
+    if url is None or not isinstance(url, str):
+        msg = f"{request.remote_addr}: Missing or invalid url field"
+        app.logger.warning(msg)
+        write_log(msg, "ERROR")
+        return jsonify({"error": "missing/invalid url field"}), 400
+    
+    # check if url is valid format 
     if not check_valid_url(url):
-        app.logger.warning(f"Bad URL from {request.remote_addr}: {url[:100]}...")
-        return jsonify({"error": "invalid url format"}), 400
+        msg = f"Bad URL from {request.remote_addr}: {url}"
+        app.logger.warning(msg)
+        write_log(msg, "ERROR")
+        return jsonify({"error": "bad URL format"}), 400
 
     app.logger.info(type(request_data))
 
