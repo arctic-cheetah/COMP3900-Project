df9bec31afd628f6a1ccb6b7fb57cf561e40a01a
kelly <kellyfirstname@gmail.com>
2026-03-16 13:19:56 +1100

    fix: changed is_domain_ip function to use ipaddress library

--- backend/preprocessor.py
+++ backend/preprocessor.py
@@ -7,6 +7,8 @@ import requests
 import logging
 import json, datetime
 from pathlib import Path as path
+from ipaddress import ip_address, AddressValueError, NetmaskValueError
+
 
 # create logger for preprocessor
 logger = logging.getLogger(__name__)
@@ -28,9 +30,11 @@ class preprocess_data:
     # Page data should be list of lines for ease of processing
     page_data: List[str]
 
+
     def __init__(self, url: str):
         self.url_len = len(url)
         self.url = url
+
 
     def get_data(self) -> pd.DataFrame:
         func_pointer = [
@@ -88,7 +92,6 @@ class preprocess_data:
         #     # print((x[1], x[0](self.url)))
         #     dat[x[1]] = [x[0](self.url)]
         # return pd.DataFrame(dat)
-
     def URLLength(self, url: str):
         return len(url)
 
@@ -97,24 +100,39 @@ class preprocess_data:
         return len(domain)
 
     def IsDomainIP(self, url: str):
+        """
+            Checks if domain is an IP address.
+            
+            Args:
+                url (str): The URL to be checked.
+            
+            Returns:
+                int: Returns 1 if domain is IP, otherwise 0.
+        """
         parsed_url = urlsplit(url)
-        regexIPV4 = r"\d+\.\d+\.\d+\.\d+"
-        regexIPV6 = r"/^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/"
+        hostname = str(parsed_url.hostname)
+       
+        try:
+            ip_address("hostname")
+            return 1
+        except AddressValueError or NetmaskValueError:
+            return 0
+        except Exception as e:
+            print(f'is_domain_ip error: "{e}"')
 
-        ipv4 = re.match(regexIPV4, str(parsed_url.hostname))
-        ipv6 = re.match(regexIPV6, str(parsed_url.hostname))
-        return 1 if (ipv6 is None) or (ipv4 is None) else 0
 
     def TLDLength(self, url: str):
         extracted = tldextract.extract(url)
         tld = extracted.suffix
         return len(tld)
 
+
     def NoOfSubDomain(self, url: str):
         extracted = tldextract.extract(url)
         if extracted.subdomain == "":
             return 0
         return len(extracted.subdomain.split("."))
+
 
     def HasObfuscation(self, url: str):
         # URL obfuscation according to this article
@@ -124,6 +142,7 @@ class preprocess_data:
         regex = r"%[0-9a-fA-F]{2}"
         return 1 if re.match(regex, url) is None else 0
 
+
     def NoOfObfuscatedChar(self, url: str):
         regex = r"%[0-9a-fA-F]{2}"
         found = re.findall(regex, url)
@@ -132,8 +151,10 @@ class preprocess_data:
         self.numObfuscatedChar = len(found) * 3
         return self.numObfuscatedChar
 
+
     def ObfuscationRatio(self, url: str):
         return self.NoOfObfuscatedChar(url) / len(url) if len(url) > 0 else 0
+
 
     def NoOfLettersInURL(self, url: str):
         # Count number of unique letters!
@@ -144,27 +165,35 @@ class preprocess_data:
 
         return len(ht)
 
+
     def LetterRatioInURL(self, url: str):
         return self.NoOfLettersInURL(url) / len(url) if len(url) > 0 else 0
+
 
     def NoOfDigitsInURL(self, url: str):
         return sum(c.isdigit() for c in url)
 
+
     def DigitRatioInURL(self, url: str):
         return self.NoOfDigitsInURL(url) / len(url) if len(url) > 0 else 0
+
 
     def NoOfEqualsInURL(self, url: str):
         return sum(c in "=" for c in url)
 
+
     def NoOfQMarkInURL(self, url: str):
         return sum(c in "?" for c in url)
+
 
     def NoOfAmpersandInURL(self, url: str):
         return sum(c in "&" for c in url)
 
+
     def NoOfOtherSpecialCharsInURL(self, url: str):
         special = "!@#$%^*()_+-[]{}|;:'\",<>~`"
         return sum(c in special for c in url)
+
 
     def SpacialCharRatioInURL(self, url: str):
         total_special = (
@@ -175,6 +204,7 @@ class preprocess_data:
         )
 
         return total_special / len(url) if len(url) > 0 else 0
+
 
     def IsHTTPS(self, url: str):
         return 1 if url.strip().lower().startswith("https://") else 0
