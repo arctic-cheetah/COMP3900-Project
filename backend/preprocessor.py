4dc713048db851a011044fe99647b693362d6c69
arctic-cheetah <gondonlondon@gmail.com>
2026-03-27 01:12:10 +1100

    fix: added code to prevent division by zero in preprocessor and added this avoidance of division by zero to functions that require division!

--- backend/preprocessor.py
+++ backend/preprocessor.py
@@ -87,7 +87,7 @@ class preprocess_data:
         return pd.DataFrame(data)
 
     def _avoid_div_zero(self, url: str) -> int:
-        return max(len(url), 1)
+        return max(len(url) - 1, 1)
 
     def url_length(self, url: str):
         return len(url)
@@ -150,7 +150,7 @@ class preprocess_data:
 
 
     def obfuscation_ratio(self, url: str):
-        return self.no_of_obfuscated_char(url) / len(url) if len(url) > 0 else 0
+        return self.no_of_obfuscated_char(url) / self._avoid_div_zero(url)
 
 
     def no_of_letters_in_url(self, url: str):
@@ -209,31 +209,11 @@ class preprocess_data:
             + self.no_of_other_special_chars_in_url(url)
         )
 
-        return total_special / len(url) if len(url) > 0 else 0
+        return total_special / self._avoid_div_zero(url)
 
 
     def is_https(self, url: str):
         return 1 if url.strip().lower().startswith("https://") else 0
-    
-    
-    # TODO: ASK+CHECK WITH KELLY ABOUT THESE TWO FIELDS
-    # IF U CANNOT FETCH FROM WEBSITE THEN IT SHOULD RETURN FALSE
-    # def LineOfCode(self, url: str):
-    #     # TODO: REDIRECTS ARE BAD HERE
-    #     try:
-    #         r = requests.get(
-    #             url, allow_redirects=True, timeout=10, headers=self.headers
-    #         )
-    #         self.page_data = r.text.splitlines()
-    #         return len(r.text.splitlines())
-    #     except Exception as err:
-    #         print(err)
-    #         self.page_data = []
-    #         return 0
-    #         # Check if request failed!
-
-    # def LargestLineLength(self, url: str):
-    #     return max((len(line) for line in self.page_data), default=0)
 
 
 # TODO: Gotta run the class
