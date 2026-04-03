28a05bc762dd3ce932b6afbdc3897b74f10ed24f
Kelly Chen <z5476337@ad.unsw.edu.au>
2026-04-03 20:37:13 +1100

    feat: increase speed of searching through whitelist

--- backend/ml/pipeline.py
+++ backend/ml/pipeline.py
@@ -38,35 +38,50 @@ def run_model(url_features: pd.DataFrame, model_path: str) -> tuple[str, str] | 
 
 
 #  Where 1 is identical and 0 is different.
-def normalised_levenshtein(a, b):
+def normalised_levenshtein(a: str, b: str):
     return 1 - distance(a, b) / max(len(a), len(b))
 
 
 #  Where 1 is identical and 0 is different.
-def normalised_lcs(a, b):
+def normalised_lcs(a: str, b: str):
     return (2 * lcs_sequence_length(a, b)) / (len(a) + len(b))
 
 
-def search_whitelist(domain: str, whitelist_filepath: str):
-    whitelist_file = pd.read_csv(whitelist_filepath)
-    whitelist = whitelist_file["Domain"]
+def get_whitelist(whitelist_filepath: str):
+    whitelist_df = pd.read_csv(whitelist_filepath)
+    return whitelist_df["Domain"].tolist()
 
-    best_levenshtein = 0
-    best_jaro_winkler = 0
-    best_lcs = 0
-    for whitelist_domain in whitelist:
-        try:
+
+def search_whitelist(domain: str, whitelist: list):
+    whitelist_set = set(whitelist)
+    if domain in whitelist_set:
+        return {
+            "Levenshtein": 1,
+            "JaroWinkler": 1,
+            "LCS": 1,
+        }
+
+    try:
+        best_levenshtein = 0
+        best_jaro_winkler = 0
+        best_lcs = 0
+        for whitelist_domain in whitelist:
             best_levenshtein = max(best_levenshtein, normalised_levenshtein(domain, whitelist_domain))
             best_jaro_winkler = max(best_jaro_winkler, jaro_winkler(domain, whitelist_domain))
             best_lcs = max(best_lcs, normalised_lcs(domain, whitelist_domain))
-        except Exception as e:
-            print(e)
 
-    return {
-        "Levenshtein": best_levenshtein,
-        "JaroWinkler": best_jaro_winkler,
-        "LCS": best_lcs,
-    }
+        return {
+            "Levenshtein": round(best_levenshtein, 6),
+            "JaroWinkler": round(best_jaro_winkler, 6),
+            "LCS": round(best_lcs, 6),
+        }
+    except Exception as e:
+        print(e)
+        return {
+            "Levenshtein": 0,
+            "JaroWinkler": 0,
+            "LCS": 0,
+        }
   
 
 def model_pipeline(url: str) -> tuple[str, str] | None:
@@ -85,10 +100,15 @@ def model_pipeline(url: str) -> tuple[str, str] | None:
         df = url_obj.get_data()
 
         domain = df["RootDomain"].iloc[0]
-        scores = search_whitelist(domain, whitelist_path)
+        whitelist = get_whitelist(whitelist_path)
+        scores = search_whitelist(domain, whitelist)
         if scores["Levenshtein"] == 1 and scores["JaroWinkler"] == 1 and scores["LCS"] == 1:
             return 1, [0, 100]
-            
+
+        df["Levenshtein"] = scores["Levenshtein"]
+        df["JaroWinkler"] = scores["JaroWinkler"]
+        df["LCS"] = scores["LCS"]
+
         is_safe, confidence = run_model(df, model_path)
 
         return is_safe, confidence
