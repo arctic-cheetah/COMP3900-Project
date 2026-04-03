a8ed031c3734a7b112cd369e4093a0714cff85f6
arctic-cheetah <gondonlondon@gmail.com>
2026-04-04 03:22:10 +1100

    feat: added model with feature vars that I trained

--- backend/preprocessor.py
+++ backend/preprocessor.py
@@ -172,6 +172,7 @@ class preprocess_data:
         """
         # TODO: REDIRECTS ARE BAD HERE
         # TODO: FIX TIMEOUT
+        # TODO: NEED TO SWITCH FROM REQUESTS TO SELENIUM BROWSER API
         # Check if request failed!
         try:
             r = requests.get(
