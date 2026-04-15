83341ed5d14d824745e9a8b02b366585295288b5
Caitlin Dang <z5416447@student.unsw.edu.au>
2026-04-16 00:59:57 +1000

    Tests started failing so updates setupTests and removed babel from dependencies cause we dont use anymore

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,1 +1,24 @@
 import "@testing-library/jest-dom";
+
+// jsdom doesn't fully support the test environment so this
+// prevents tets from crashing
+const localStorageMock = (() => {
+    let store = {};
+
+    return {
+        getItem: (key) => store[key] || null,
+        setItem: (key, value) => {
+            store[key] = String(value);
+        },
+        removeItem: (key) => {
+            delete store[key];
+        },
+        clear: () => {
+            store = {};
+        },
+    };
+})();
+
+Object.defineProperty(window, "localStorage", {
+    value: localStorageMock,
+});
